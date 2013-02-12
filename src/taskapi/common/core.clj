3498fa2797bbaac372ca4eeadba43adf8747f9bd
Will Fuqua <wafuqua@gmail.com>
2013-02-11 20:01:53 -0500

    code comments

--- src/taskapi/common/core.clj
+++ src/taskapi/common/core.clj
@@ -3,5 +3,7 @@
 ; given a map m, a function f, and keys ks
 ; apply f to the values in m that correspond with ks
 (defn map-selected-vals [m f & ks]
-  (reduce #(update-in %1 [%2] f) m ks))
+  (let [submap (select-keys m ks)
+        transformed (into {} (for [[k v] submap] [k (f v)]))]
+    (merge m transformed)))
 
